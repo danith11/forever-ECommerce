20a4c1b0aea34a311575ffc4ad95e42853b0b405
Yethum Danith <yethumdanith2002@gmail.com>
2025-06-16 06:43:25 +0530

    Added Verify page

--- src/pages/Verify.jsx
+++ src/pages/Verify.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useContext } from "react";
 import { ShopContext } from "../context/ShopContext";
 import { useSearchParams } from "react-router-dom";
+import { toast } from "react-toastify";
+import  axios  from "axios";
 
 const Verify = () => {
   const { navigate, token, setCartItems, backendUrl } = useContext(ShopContext);
@@ -9,7 +11,31 @@ const Verify = () => {
   const success = searchparms.get("success");
   const orderId = searchparms.get("orderId");
 
-  const verifyPayment = async () => {};
+  const verifyPayment = async () => {
+    try {
+      if (!token) {
+        return null;
+      }
+      const response = await axios.post(
+        backendUrl + "/api/order/verifyStripe",
+        { success, orderId },
+        { headers: { token } }
+      );
+      if (response.data.success) {
+        setCartItems({});
+        navigate("/orders");
+      } else {
+        navigate("/cart");
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    verifyPayment();
+  }, [token]);
   return <div></div>;
 };
 
